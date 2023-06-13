786973a8d4b38d9fb6293a340f115b687d452804
Will Patterson <wpatterson@shutterstock.com>
2023-06-13 14:55:02 -0600

    Add health check path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { config } from "dotenv-flow";
+import express from "express";
 
 config();
 
 import { initialize } from "./bot";
 
 initialize();
+
+const app = express();
+
+app.get("/health", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(Number(process.env.PORT || 3000));
