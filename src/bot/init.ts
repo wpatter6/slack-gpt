93e24b5932f773d2fdb2c496f60983abfc330ac5
Will Patterson <wpatterson@shutterstock.com>
2023-06-13 14:58:03 -0600

    Add health check

--- src/bot/init.ts
+++ src/bot/init.ts
@@ -7,6 +7,16 @@ export const initApp = () => {
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     token: process.env.SLACK_BOT_TOKEN,
     port,
+    customRoutes: [
+      {
+        path: "/health",
+        method: ["GET"],
+        handler: (req, res) => {
+          res.writeHead(200);
+          res.end("Health check information displayed here!");
+        },
+      },
+    ],
   });
 
   return app;
